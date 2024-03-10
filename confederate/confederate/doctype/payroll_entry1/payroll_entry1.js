b9ccb032dc43d736ae7598f9a15327a6aaba839f
root <root@ip-172-31-46-219.ap-south-1.compute.internal>
2024-03-10 09:41:32 +0000

    initial commit

---
+++ confederate/confederate/doctype/payroll_entry1/payroll_entry1.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Safeer C P and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Payroll Entry1', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ confederate/confederate/doctype/payroll_entry1/test_payroll_entry1.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Safeer C P and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPayrollEntry1(unittest.TestCase):
+	pass
